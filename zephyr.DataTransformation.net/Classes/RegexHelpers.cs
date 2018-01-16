dc1b4b77a42ad37391db93a92f9830f99ec204f8
Steve <steve.shortt@gmail.com>
2018-01-15 23:21:14 -0600

    added Regex

---
+++ zephyr.DataTransformation.net/Classes/RegexHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Zephyr.DataTransformation
+{
+    public class RegexHelpers
+    {
+        public static Match Match(string input, string pattern, RegexOptions options, TimeSpan matchTimeout)
+        {
+            return Regex.Match( input, pattern, options, matchTimeout );
+        }
+
+        public static MatchCollection Matches(string input, string pattern, RegexOptions options, TimeSpan matchTimeout)
+        {
+            return Regex.Matches( input, pattern, options, matchTimeout );
+        }
+    }
+}
